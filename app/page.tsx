7758d35e1f2486e0b84ad0ed086abee48747a374
Master Systems EU - Team <jfelk.92201@gmail.com>
2025-07-25 16:36:41 -0500

    reverseBanner
    reverseBanner

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Link from 'next/link';
 export default function Home() {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const [isBannerPaused, setIsBannerPaused] = useState(false);
 
   const bannerImages = [
     { url: "https://i.ibb.co/mCG7xd6C/1.jpg", title: "", description: "" },  //OpenHouse
@@ -21,12 +20,11 @@ export default function Home() {
     ];
 
   useEffect(() => {
-    if (isBannerPaused) return;
     const interval = setInterval(() => {
       setCurrentImageIndex((prevIndex) => prevIndex === bannerImages.length - 1 ? 0 : prevIndex + 1);
     }, 6000);
     return () => clearInterval(interval);
-  }, [bannerImages.length, isBannerPaused]);
+  }, [bannerImages.length]);
 
   const nextImage = () => {
     setCurrentImageIndex((prevIndex) => prevIndex === bannerImages.length - 1 ? 0 : prevIndex + 1);
@@ -206,12 +204,7 @@ export default function Home() {
 
       {/* Banner Section */}
       <section className="pt-20 relative">
-        <div
-          className="relative h-96 md:h-[650px] overflow-hidden w-full md:w-[80%] mx-auto"
-          onMouseDown={() => setIsBannerPaused(true)}
-          onMouseUp={() => setIsBannerPaused(false)}
-          onMouseLeave={() => setIsBannerPaused(false)}
-        >
+        <div className="relative h-96 md:h-[650px] overflow-hidden w-full md:w-[80%] mx-auto">
           {bannerImages.map((image, index) => (
             <div key={index} className={`absolute inset-0 transition-opacity duration-600 ${index === currentImageIndex ? 'opacity-100' : 'opacity-0'}`}>
               <img src={image.url} alt={image.title} className="w-full h-full object-cover object-center" />
