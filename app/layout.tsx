e2d7eec28997ef42cabaf6ba70697d865737b109
Master Systems EU - Team <jfelk.92201@gmail.com>
2025-08-06 09:30:29 -0500

    TODO FUNCIONAL
    TODO FUNCIONAL 06/08/2025

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pacifico } from "next/font/google";
 import "./globals.css";
 
+
 const pacifico = Pacifico({
   weight: '400',
   subsets: ['latin'],
